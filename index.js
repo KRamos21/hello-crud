c3d48cff34ea3cbc00d82879b2f72a058b0072da
Kauê Ramos Raimundo <kaue.ramos.krr@gmail.com>
2021-05-27 22:43:09 -0300

    I added the post, put, and delete features

--- index.js
+++ index.js
@@ -5,14 +5,49 @@ app.use(express.json());
 const port = 3000;
 
 const usuarioFernando = {
-  id: 1,
+  id: 0,
   nome: "Fernando",
   idade: 35,
-}
+};
 
 const usuarios = [usuarioFernando];
 
 app.get("/usuarios", (req, res) => res.json(usuarios));
+
+app.post("/usuarios", (req, res) => {
+  const { nome, idade } = req.body;
+  const id = usuarios.length;
+
+  const novoUsuario = {
+    id,
+    nome,
+    idade,
+  };
+
+  usuarios.push(novoUsuario);
+  res.status(201).json(novoUsuario);
+});
+
+app.put("/usuarios/:id", (req, res) => {
+  const { id } = req.params;
+  const { nome, idade } = req.body;
+
+  const usuario = usuarios.find((usuario) => usuario.id === Number(id));
+  usuario.nome = nome;
+  usuario.idade = idade;
+
+  res.json(usuario);
+});
+
+app.delete("/usuarios/:id", (req, res) => {
+  const { id } = req.params;
+
+  const index = usuarios.findIndex((usuario) => usuario.id === Number(id));
+
+  usuarios.splice(index, 1);
+
+  res.status(204).send();
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
